7acda4c42560b4a7d9cbb19a31d225a1fe85b225
Ravindra Sojitra <rsojitra@splunk.com>
2021-08-02 15:55:11 +0530

    PAPP-18845: Minor changes.

--- lastline_consts.py
+++ lastline_consts.py
@@ -21,7 +21,7 @@ VAULT_ERR_INVALID_VAULT_ID = "Invalid Vault ID"
 VAULT_ERR_FILE_NOT_FOUND = "Vault file could not be found with supplied Vault ID"
 
 PHANTOM_ERR_CODE_UNAVAILABLE = "Error code unavailable"
-PHANTOM_ERR_MSG_UNAVAILABLE = "Unknown error occurred. Please check the asset configuration and|or action parameters."
+PHANTOM_ERR_MSG_UNAVAILABLE = "Unknown error occurred. Please check the asset configuration and|or action parameters"
 
 LASTLINE_GENERATED_RANDOM_HASH = "Generated Random hash '{gen_hash}' to query"
 LASTLINE_SUCC_CONNECTIVITY_TEST = "Test Connectivity Passed"
@@ -30,10 +30,10 @@ LASTLINE_ERR_CONNECT = "Error occurred, response: {response}"
 LASTLINE_ERR_GETTING_REPORT = "Error getting report"
 LASTLINE_ERR_SUBMIT_URL = "Error submitting URL"
 LASTLINE_ERR_TASK_ID_NOT_FOUND = "Task ID not found"
-LASTLINE_ERR_NO_FILES = "No runnable files found."
+LASTLINE_ERR_NO_FILES = "No runnable files found"
 
 LASTLINE_SLEEP_SECS = 10
 LASTLINE_JSON_POLL_TIMEOUT_SECS = "timeout"
 LASTLINE_MAX_TIMEOUT_DEF_SECS = 5 * 60
-LASTLINE_POLL_TIMEOUT = "Polled for the maximum number of times."
+LASTLINE_POLL_TIMEOUT = "Polled for the maximum number of times"
 LASTLINE_POLL_TIMEOUT += " Report not yet ready. Please try 'get report' action with id: {0} later"
