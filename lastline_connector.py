ff3cde2b6a814ddd6ca2a2e28b6e0159aed85416
Ishan Shah <ishans@splunk.com>
2021-09-07 11:49:00 +0530

    added support for last char as / for result url

--- lastline_connector.py
+++ lastline_connector.py
@@ -48,6 +48,7 @@ class LastlineConnector(BaseConnector):
         )
         self._account_name = config.get('account_username')
         self._report_url = config.get('report_url', 'https://user.lastline.com')
+        self._report_url = self._report_url if self._report_url[-1] != '/' else self._report_url[:-1]
         self.RESULTS_URL_TEMPLATE = self._report_url + '/portal#/analyst/task/{}'
 
         return phantom.APP_SUCCESS
